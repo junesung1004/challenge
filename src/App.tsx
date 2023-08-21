0f4dc9caa3e33d9606a0f24483ad17541c5130ab
junesung1004 <135226639+junesung1004@users.noreply.github.com>
2023-08-21 18:17:12 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-// components
-
-// pages
-import ListPage from './pages/list_page';
-import NewPage from './pages/new_page';
-import LoginPage from './pages/login/login_page';
-import JoinPage from './pages/join/join_page';
+import './reset.css';
+import { ROUTE_ARR } from './routes';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/newpage" element={<NewPage />} />
-      <Route path="/listpage" element={<ListPage />} />
-      <Route path="/loginpage" element={<LoginPage />} />
-      <Route path="/joinpage" element={<JoinPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        {ROUTE_ARR.map((route, index) => {
+          return (
+            <Route path={route.path} element={<route.element />} key={index} />
+          );
+        })}
+      </Routes>
+    </div>
   );
 }
 
